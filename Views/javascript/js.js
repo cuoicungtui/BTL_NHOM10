1650d6d6a2bf57e91700e9bee9af94b036999471
ntv01 <ngotung180301@gmail.com>
2022-03-06 17:57:13 +0700

    update

--- Views/javascript/js.js
+++ Views/javascript/js.js
@@ -1,7 +1,17 @@
 var avatarWrap = document.querySelector('.avatar__wrap')
 var navImg = document.querySelector('.nav__img')
+var modal = document.querySelector('.modal')
 
 navImg.addEventListener('click', ()=>{
   avatarWrap.classList.toggle('hide')
 })
+
+// modal.addEventListener('click', (e)=> {
+//   if(e.target == e.currentTarget) {
+//     modal.classList.toggle('hide')
+//   }
+// })
+var modal1 = document.querySelector('.modal1')
+var navModal1 = document.querySelector('.nav-modal1')
+
 
